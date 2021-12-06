4296038cbeaeaa3faad7c212adb6e552e15096e5
Henry Rocha <henryrocha@protonmail.com>
2021-12-06 19:32:04 -0300

    chore: add threejs and svelte cubed

--- svelte.config.js
+++ svelte.config.js
@@ -16,6 +16,12 @@ const config = {
 
         // hydrate the <div id="svelte"> element in src/app.html
         target: "#svelte",
+
+        vite: {
+            ssr: {
+                noExternal: ["three"],
+            },
+        },
     },
 };
 
